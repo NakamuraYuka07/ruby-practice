8a81ae263942b1b1e50460495e42b14df8307236
NakamuraYuka07 <yukaron.8107@gmail.com>
2022-01-17 17:34:46 +0900

    メソッドについて

--- hello.rb
+++ hello.rb
@@ -1,2 +1,11 @@
 #DMMの振り返り
 puts "Hello,World!"
+
+#文字数数える
+puts "Hello,World!".length
+#文字を逆に表示
+puts "Hello,World!".reverse
+#引数に指定した文字列が含まれているかどうかを調べる
+puts "WEBCAMPでプログラミング学習".include?("WEBCAMP")
+#小文字を大文字に
+puts "webcampでプログラミング学習".upcase
